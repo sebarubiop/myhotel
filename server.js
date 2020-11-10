05db0d2a9b4d68fcf1ce4ce21c830651482641bc
Sebastian Rubio <sebarubiop@gmail.com>
2020-11-10 16:00:52 -0300

    adding a server file to deploy in heroku

--- server.js
+++ server.js
@@ -2,30 +2,11 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
-
-// Get our API routes
-const api = require('./server/routes/personas');
 
 const app = express();
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist/tech-lead')));
-
-// Set our api routes
-app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
